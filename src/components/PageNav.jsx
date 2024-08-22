53db9423a2ce2487ab2a064a83a98299514ec4b2
yonathandevpro <yonathanber@icloud.com>
2024-08-22 14:50:56 +0300

    implementing css modules

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <div>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -14,7 +15,7 @@ function PageNav() {
           <NavLink to="/product">Product</NavLink>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
