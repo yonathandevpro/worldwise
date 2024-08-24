d62d02afff4cbdcbc096a84a392b79cf0c8f72ff
yonathandevpro <yonathanber@icloud.com>
2024-08-24 17:55:22 +0300

    Protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
@@ -23,7 +24,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route
                 index
                 element={<Navigate replace to="cities"></Navigate>}
